4294bbd770fd48a455bac1e2961a1529385fe843
Oli Oskarsson <oli@oskarsson.nl>
2014-10-28 00:25:53 +0100

    Upgraded to datatables 1.10.3

--- package.js
+++ package.js
@@ -1,27 +1,28 @@
 Package.describe({
-    summary: "DataTable - jQuery plugin for sortable, pagable data",
+    summary: "jQuery DataTables with Bootstrap3 JS & CSS",
     name: "loftsteinn:datatables-bootstrap3",
-    version: "0.2.0",
+    version: "1.10.3",
     git: "https://github.com/oskarszoon/meteor-datatables-bootstrap3"
 });
 
 Package.on_use(function (api) {
-  api.use('jquery@1.0.0', 'client');
-  api.use('ian:bootstrap-3@1.0.2', 'client');
-  api.add_files([
-    'lib/jquery.dataTables.js',
-    'lib/datatables.js',
-    'lib/jquery.dataTables.css',
-    'lib/datatables.css',
-    'lib/images/sort_asc.png',
-    'lib/images/sort_asc_disabled.png',
-    'lib/images/sort_both.png',
-    'lib/images/sort_desc.png',
-    'lib/images/sort_desc_disabled.png',
-  ], 'client');
+    api.use('jquery@1.0.0', 'client');
+    // disabled so user can choose own bootstrap library
+    //api.use('ian:bootstrap-3@1.0.3', 'client');
+    api.add_files([
+        'lib/jquery.dataTables.js',
+        'lib/datatables.js',
+        'lib/jquery.dataTables.css',
+        'lib/datatables.css',
+        'lib/images/sort_asc.png',
+        'lib/images/sort_asc_disabled.png',
+        'lib/images/sort_both.png',
+        'lib/images/sort_desc.png',
+        'lib/images/sort_desc_disabled.png',
+    ], 'client');
 });
 
-Package.on_test( function(api) {
+Package.on_test(function (api) {
     api.use([
         'loftsteinn:datatables-bootstrap3',
         'test-helpers',
